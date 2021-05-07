ad5889b904aee3bddaea06b34d20b20a58c28739
Roger <rogermanzo04@gmail.com>
2021-05-07 09:54:20 -0400

    guacamole

---
+++ script.py
@@ -0,0 +1,42 @@
+import os
+import cv2
+import json
+import keras
+import string
+import numpy as np
+#from breaker import predict
+from keras.models import load_model
+
+model = load_model('predict.h5')
+
+#model.summary()
+#model.get_weights()
+symbols = string.ascii_lowercase + "0123456789" # All symbols captcha can contain
+
+# Define function to predict captcha
+def predict(filepath):
+    img = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+    if img is not None:
+        img = img / 255.0
+    else:
+        print("Not detected");
+    res = np.array(model.predict(img[np.newaxis, :, :, np.newaxis]))
+    ans = np.reshape(res, (6, 36))
+    l_ind = []
+    probs = []
+    for a in ans:
+        l_ind.append(np.argmax(a))
+        #probs.append(np.max(a))
+
+    capt = ''
+    for l in l_ind:
+        capt += symbols[l]
+    return capt#, sum(probs) / 5
+
+# Lets Predict By Model
+text = predict('./captcha1.png')
+print("Predicted Captcha =",predict('./captcha1.png'))
+
+#save text into captcha.json
+with open('captcha.json', 'w') as f:
+    json.dump(text, f)
---
+++ test.py
@@ -0,0 +1,40 @@
+import cv2
+import time
+import numpy as np
+from PIL import Image
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+x = 1
+
+while x <= 10:
+
+    opt = webdriver.ChromeOptions()
+
+    opt.add_argument("--headless")
+
+    browser = webdriver.Chrome(executable_path=r"/home/rhino/opt/chromedriver", options=opt)
+
+    browser.get('http://contribuyente.seniat.gob.ve/BuscaRif/Captcha.jpg')
+
+    captcha = browser.find_element_by_xpath('/html/body/img')
+
+    screenshot = captcha.screenshot_as_png
+    with open('captcha%s.png' % x, 'wb') as f:
+        f.write(screenshot) 
+    
+    img = Image.open('captcha%s.png' % x)
+    img = img.resize((200, 50), Image.ANTIALIAS)
+    img = cv2.imread('./captcha%s.png' % x, 0)
+    kernel = np.ones((2),np.uint8)
+    erosion = cv2.erode(img,kernel,iterations = 2)
+    _,binarizadaInv = cv2.threshold(erosion,95,250,cv2.THRESH_BINARY_INV)
+    #cv2.imwrite('captcha%s.png' % x, binarizadaInv)
+
+    time.sleep(2)
+
+    #browser.close()
+
+    x+=1
